0e90a96d80aab03886b5c9c2b5ad80cdeedcc768
mooreEffort <alexmaksimets@gmail.com>
2022-12-03 15:23:14 +1000

    file structure updated

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// translate input (english text) into morse code
+
+// 1 space between english WORDS
+
+// translate input (morse code) into english
+
+// 1 space between morse CHARACTERS
+
+// Bonus:
+// - handle special characters
+// - detect if morse code / english and translate
+
+// edge cases?
